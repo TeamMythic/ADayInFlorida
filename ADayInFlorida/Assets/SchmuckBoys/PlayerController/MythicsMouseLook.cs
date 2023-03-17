440ff217d5ba26883294d239b88d65b606d7cf78
TeamMythic <mythicgaming234@gmail.com>
2023-03-17 00:55:42 -0700

    added player controller

---
+++ ADayInFlorida/Assets/SchmuckBoys/PlayerController/MythicsMouseLook.cs
@@ -0,0 +1,24 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MythicsMouseLook : MonoBehaviour
+{
+	public float mouseSensitivity = 100f;
+	[SerializeField] private Transform playersBody;
+	private float xRotation = 0f;
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	private void Update()
+	{
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+		xRotation -= mouseY;//every frame decrease x rotation by mouse y:
+		xRotation = Mathf.Clamp(xRotation, -90, 90);
+		transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		playersBody.Rotate(Vector3.up * mouseX);
+	}
+}
---
+++ ADayInFlorida/Assets/SchmuckBoys/PlayerController/MythicsPlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MythicsPlayerMovement : MonoBehaviour
+{
+    private CharacterController playersCharacterController;
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float walkSeed = 12;
+    [SerializeField] private float runSpeed = 20f;
+	[SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float gravityMagnitude = 5f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playersCharacterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleGrounded();
+		HandleInput();
+        velocity.y += gravity * Time.deltaTime;
+        playersCharacterController.Move((gravityMagnitude * velocity) * Time.deltaTime);//△y = (1/2)gravity * (time)^2
+	}
+    private void HandleGrounded()
+    {
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+		if (isGrounded && velocity.y < 0)
+		{//Fix for being grounded to smoothMore:
+			velocity.y = -2f;
+		}
+	}
+    private void HandleInput()
+    {
+        HandleMouse();
+        HandleSprint();
+        HandleJump();
+	}
+    private void HandleJump()
+    {
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{//V = sqrt(h * -2 * g):
+
+			velocity.y = Mathf.Sqrt((jumpHeight) * -2 * (gravity * gravityMagnitude));
+		}
+	}
+    private void HandleSprint()
+    {
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+		{
+			speed = runSpeed;
+			return;
+		}
+		speed = walkSeed;
+	}
+    private void HandleMouse()
+    {
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+		Vector3 move = transform.right * x + transform.forward * z;
+		playersCharacterController.Move(move * speed * Time.deltaTime);
+	}
+}
